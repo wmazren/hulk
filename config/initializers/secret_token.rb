8b03574cd8f73dd97a0061cdaa7d28a06ca19cc1
Azren <w.m.azren@gmail.com>
2012-05-17 23:35:04 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hulk::Application.config.secret_token = 'b99609bc034175f2f3fc7ebd0a81e28feb2ff7177b8f33c53ec29f17df63114c43dc40bd1efdfa943a1e72e3c96d64dda729a1017bc0575825f00c391bf93246'
